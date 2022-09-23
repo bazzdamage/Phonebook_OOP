f2b582219d52deb45fed785542d55514a20391d0
bazzdamage <bazzdamage@outlook.com>
2022-09-24 04:07:53 +0700

    fix quit

--- src/ui/QuitMenu.java
+++ src/ui/QuitMenu.java
@@ -1,6 +1,12 @@
 package ui;
 
+import enums.Command;
+
 public class QuitMenu extends MenuItem{
+    public QuitMenu() {
+        header = "Bye, dear User";
+    }
+
     @Override
     public void showMenuContent() {
         System.out.println("Thanks for using this program");
